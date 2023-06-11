8f33c9f5e5c9bbd27073b81de97c26051edcb45a
Dmp-86 <danni_power69@hotmail.com>
2023-06-11 12:24:11 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Write your code to expect a terminal of 80 characters wide and 24 rows high .
+
+def greet():
+    """
+    Greeting for main menu. Gathers user name for game
+    """
+    name = (input("What is your name? \n"))
+    print ("Welcome to Wonderland Quest, " + name)
+    print ("")
+
+
+greet()
