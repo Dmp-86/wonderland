5591196b5aa784e96ed7229024495c666b2a32fc
Dmp-86 <danni_power69@hotmail.com>
2023-06-12 11:36:29 +0000

    Build crossroads function

--- run.py
+++ run.py
@@ -2,19 +2,25 @@
 import string
 import colorama
 from colorama import Fore, Back, Style
-colorama.init(autoreset=True)
+colorama.init(autoreset=True) #Resets colorama 
 
 def crossroads():
     """
-    
+    Define crossroads function
     """
+    print ("You find yourself at a crossroads. You know you're close to Wonderland..")
+    print ("Which way would you like to go?")
+    print ("Left takes you to the talking owl. Right takes you to the Cave.")
+    print ("Forward brings you to sleepy Sasquatch")
+    print (Fore.RED + "Type left, right or forward to continue")
+    crossroads = (input("Which direction you would like to travel? \n"))
+    if 
 
 
 def validate_name(name):
     """
     Validate a name entry to only allow names with valid letters.
     """
-    
     for char in name:
         if char not in string.ascii_letters:
             return False
@@ -26,7 +32,6 @@ def greeting():
     """
     Greeting for main menu. Gathers user name for game
     """
-    
     print ("Welcome to Wonderland Quest!")
     print ("You're about to embark on a magical journey through a mystical land...")
     print ("Can you make it to the forest to .....?")
@@ -35,9 +40,10 @@ def greeting():
     print ("Let's start with your name. What shall we call you?")
     name = (input("Enter your name \n"))
     if validate_name(name):
-        print (Fore.GREEN + "Welcome to the Quest, " + name)
+        return (Fore.GREEN + "Welcome to the Quest, " + name)
     else:
-        print(Fore.RED + "Invalid name! Please enter a valid name.")
+        print (Fore.RED + "Invalid name! Please enter a valid name.")
     
 
-greeting()
+crossroads()
+# greeting()
