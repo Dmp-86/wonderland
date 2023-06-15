b11b94f0d1de18aad7cffa1881b4b14a3e6073b9
Dmp-86 <danni_power69@hotmail.com>
2023-06-15 16:00:27 +0000

    Fix broken error handling on greeting

--- run.py
+++ run.py
@@ -34,17 +34,20 @@ def greeting():
     """
     Greeting for main menu. Gathers user name for game
     """
-    print ("Welcome to Wonderland Quest!")
-    print ("You're about to embark on a magical journey through a mystical land...")
-    print ("Can you make it to the forest to .....?")
-    print ("Follow the prompts and choose which direction you want to travel.")
-    print ("But BEWARE! Not everything is as it seems...\n")
-    print ("Let's start with your name. What shall we call you?")
-    name = (input("Enter your name \n"))
-    if validate_name(name):
-        print (Fore.GREEN + "Welcome to the Quest, " + name)
-    else:
-        print (Fore.RED + "Invalid name! Please enter a valid name.")
+    while True:
+        print ("Welcome to Wonderland Quest!")
+        print ("You're about to embark on a magical journey through a mystical land...")
+        print ("Can you make it to the forest to .....?")
+        print ("Follow the prompts and choose which direction you want to travel.")
+        print ("But BEWARE! Not everything is as it seems...\n")
+        print ("Let's start with your name. What shall we call you?")
+        name = (input("Enter your name \n"))
+        clear()
+        if validate_name(name):
+            print (Fore.GREEN + "Welcome to the Quest, " + name)
+            break
+        else:
+            print (Fore.RED + "Invalid name! Please enter a valid name.")
     
 
 
